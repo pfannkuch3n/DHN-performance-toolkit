15643a6044aa0ad0b57fa69b5f047960b9c3c948
Kopka <rka-lko@eonerc.rwth-aachen.de>
2025-06-22 23:46:28 +0200

    dev script

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,4 +2,16 @@
 
 from .decentral_pump_power import calculate_decentral_pump_energy
 
-__all__ = ['calculate_decentral_pump_energy']
+from .heat_pump_analyzer import (
+    calculate_energy_from_power,
+    analyze_monthly_energy_cop,
+    plot_monthly_energy_analysis)
+
+from .utils import (
+    load_pipe_catalog)
+
+__all__ = ['calculate_decentral_pump_energy',
+           'calculate_energy_from_power',
+           'analyze_monthly_energy_cop',
+           'plot_monthly_energy_analysis',
+           'load_pipe_catalog']
